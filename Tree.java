85927858b5b32d44312f7b90c954ea29aa4725d3
Zeu Sul <sulzeu98@gmail.com>
2026-08-17 15:17:25 +1000

    additions to Tree class

--- Tree.java
+++ Tree.java
@@ -1,3 +1,28 @@
 public class Tree{
-    
+    int x;
+    int y;
+    int maxAge;
+    int age;
+    boolean isBurning;
+
+    public Tree(int x, int y, int maxAge) {
+        this.x = x;
+        this.y = y;
+        this.maxAge = maxAge;
+        this.age = 0;
+        this.isBurning = false;
+    }
+
+    void growing() {
+        if (this.age < this.maxAge) {
+            this.age++;
+        }
+    }
+
+    void burning() {
+        if (this.age > 0) {
+            this.age--;
+        }
+    }
+
 }
