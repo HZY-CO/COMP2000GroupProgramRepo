c8514920ed05c1b2ad6c2fac666f1f9fe8ee0344
HZY-CO <edrick.han@students.mq.edu.au>
2026-08-17 14:25:28 +1000

    Update App.java
    Backup

--- App.java
+++ App.java
@@ -1,5 +1,15 @@
+
+import javax.swing.*;
+
 public class App {
     public static void main(String[] Args) throws Exception{
-        
+        JFrame frame = new JFrame("Test App");
+        JPanel panel = new JPanel(){
+
+            @Override
+            protected void paintComponent(Graphics g){
+                g.fillRect(50,50,100,100);
+            }
+        }
     }
 }
