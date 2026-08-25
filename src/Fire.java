6677a62b91359781c4e703c6b8e2558be18f15cc
Ansh-verma324 <ansh.verma@students.mq.edu.au>
2026-08-25 13:35:08 +1000

    Added more Variables in the Fire Class

--- src/Fire.java
+++ src/Fire.java
@@ -1,9 +1,14 @@
 public class Fire extends Entity {
     int duration;
+    int intensity;
+    boolean burning;
+    static final int SPREAD_RADIUS = 1;
 
-    public Fire(int x, int y, int duration) {
+    public Fire(int x, int y, int duration, int intensity) {
         super(x, y);
         this.duration = duration;
+        this.intensity = intensity;
+        this.burning = true;
     }
 
     void spread(){
