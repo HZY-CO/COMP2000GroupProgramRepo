bb5b44ba0d01b864b4a75ee77c37b248dc1b3d73
HZY <edrick.han@students.mq.edu.au>
2026-08-31 17:32:14 +1000

    Initial Setup Frame
    Initial code for the Setup Frame. I intend to make the simulation size-able during setup. E.g. let the user input how many rows and columns they would like along with the cell size.

--- src/App.java
+++ src/App.java
@@ -4,13 +4,28 @@ import java.awt.*;
 
 public class App {
     public static void main(String[] Args) throws Exception{
-        JFrame frame = new JFrame("Test App");
-        JPanel panel = new JPanel(){
+        final int setupFrameWidth = 500;
+        final int setupFrameHeight = 700;
 
-            @Override
-            protected void paintComponent(Graphics g){
-                g.fillRect(50,50,100,100);
-            }
-        };
-    }
+        Frame setupFrame = new Frame("Simulation Setup");
+
+        setupFrame.setSize(setupFrameWidth, setupFrameHeight);
+        setupFrame.setLayout(null);
+        setupFrame.setBackground(new Color(40,40,40));
+        
+        Label title = new Label("Simulation Setup", Label.CENTER);
+        title.setBounds(0,60,setupFrameWidth,80);
+        title.setBackground(new Color(80,80,80));
+        title.setFont(new Font("SansSerif", Font.BOLD, 32)); 
+        title.setForeground(new Color(255,255,255));
+
+
+        setupFrame.add(title);
+        setupFrame.setVisible(true);
+
+        Frame mainFrame = new Frame("Forest Fire Simulation");
+
+        //mainFrame
+
+    };
 }
