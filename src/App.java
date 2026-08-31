b0fee7dc73c6f0d1eec6299ab252b63acd2af74f
HZY <edrick.han@students.mq.edu.au>
2026-08-31 21:51:46 +1000

    Encapsulation

--- src/App.java
+++ src/App.java
@@ -1,31 +1,44 @@
-
-import javax.swing.*;
+//import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 public class App {
-    public static void main(String[] Args) throws Exception{
+    public static void main(String[] Args) throws Exception {
         final int setupFrameWidth = 500;
         final int setupFrameHeight = 700;
+        simSetup(setupFrameWidth, setupFrameHeight);
 
+        Frame mainFrame = new Frame("Forest Fire Simulation");
+
+        // mainFrame
+
+    };
+
+    private static void simSetup(int setupFrameWidth, int setupFrameHeight) {
         Frame setupFrame = new Frame("Simulation Setup");
 
         setupFrame.setSize(setupFrameWidth, setupFrameHeight);
         setupFrame.setLayout(null);
-        setupFrame.setBackground(new Color(40,40,40));
-        
+        setupFrame.setBackground(new Color(40, 40, 40));
+        setupFrame.setResizable(false);
+
         Label title = new Label("Simulation Setup", Label.CENTER);
-        title.setBounds(0,60,setupFrameWidth,80);
-        title.setBackground(new Color(80,80,80));
-        title.setFont(new Font("SansSerif", Font.BOLD, 32)); 
-        title.setForeground(new Color(255,255,255));
+        title.setBounds(0, 60, setupFrameWidth, 80);
+        title.setBackground(new Color(80, 80, 80));
+        title.setFont(new Font("SansSerif", Font.BOLD, 32));
+        title.setForeground(new Color(255, 255, 255));
+
+        //Panel //create function for attributes
 
 
         setupFrame.add(title);
         setupFrame.setVisible(true);
 
-        Frame mainFrame = new Frame("Forest Fire Simulation");
-
-        //mainFrame
+        setupFrame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent windowEvent) {
+                System.exit(0);
+            }
+        });
 
     };
 }
