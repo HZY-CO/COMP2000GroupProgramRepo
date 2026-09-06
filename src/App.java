f68af3515749ada7a47da7dbb0422d233fdd4dd1
HZY <edrick.han@students.mq.edu.au>
2026-09-06 20:04:20 +1000

    Added height and width panels to setup.
    Still working on it, will add buttons and an update method I think to display setup values.

--- src/App.java
+++ src/App.java
@@ -1,10 +1,19 @@
+/*
+Resources
+https://docs.oracle.com/en/java/javase/26/docs/api/java.desktop/java/awt/Component.html
+https://docs.oracle.com/en/java/javase/26/docs/api/java.desktop/java/awt/package-summary.html
+*/
+
+
 //import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
 public class App {
-    private static int gridWidth = 500;
-    private static int gridHeight = 500;
+    private static int gridWidth = 16;      // The Width of the simulation by number of cells.
+    private static int gridHeight = 16;     // The Height of the simulation by number of cells.
+    private static int cellSize = 32;       // The size of each cell in pixels. Cells will be square.
+
     private static double treeGenerateProbablity = 0.7;
 
     public static void main(String[] Args) throws Exception {
@@ -18,6 +27,9 @@ public class App {
 
     };
 
+
+
+    // Method by Ed, Creates a simple frame to take inputs from the user about how the simulation will appear.
     private static void simSetup(int setupFrameWidth, int setupFrameHeight) {
         Frame setupFrame = new Frame("Simulation Setup");
 
@@ -32,9 +44,41 @@ public class App {
         title.setFont(new Font("SansSerif", Font.BOLD, 32));
         title.setForeground(new Color(255, 255, 255));
 
-        //Panel //create function for attributes
+        // Adjustable Width Attribute
+        Panel widthAttribute = new Panel();
+        widthAttribute.setLayout(null);
+        widthAttribute.setSize(setupFrameWidth, 100);
+        widthAttribute.setBackground(new Color(60, 60, 60));
+        widthAttribute.setLocation(0, 180);
 
+        Label widthAttributeText = new Label("Simulation Width (By cells)", Label.LEFT);
+        widthAttributeText.setBounds(20, 0, setupFrameWidth, 100);
+        widthAttributeText.setFont(new Font("SansSerif", Font.BOLD, 24));
+        widthAttributeText.setForeground(new Color(255, 255, 255));
+
+        widthAttribute.add(widthAttributeText);
+
+        
+        // Adjustable Height Attribute
+        Panel heightAttribute = new Panel();
+        heightAttribute.setLayout(null);
+        heightAttribute.setSize(setupFrameWidth, 100);
+        heightAttribute.setBackground(new Color(60, 60, 60));
+        heightAttribute.setLocation(0, 300);
+
+        Label heightAttributeText = new Label("Simulation Height (By cells)", Label.LEFT);
+        heightAttributeText.setBounds(20, 0, setupFrameWidth, 100);
+        heightAttributeText.setFont(new Font("SansSerif", Font.BOLD, 24));
+        heightAttributeText.setForeground(new Color(255, 255, 255));
+
+        heightAttribute.add(heightAttributeText);
+
+
+        //Compile frame content
         setupFrame.add(title);
+        setupFrame.add(widthAttribute);
+        setupFrame.add(heightAttribute);
+
         setupFrame.setVisible(true);
 
         setupFrame.addWindowListener(new WindowAdapter() {
@@ -45,6 +89,9 @@ public class App {
 
     };
 
+
+
+    //Method by Aaron,
     private static void generateForestSimulation() {
         EntityManager<Tree> treeEntityManager = new EntityManager<>();
         
