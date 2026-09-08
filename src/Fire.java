6d0ec8a62afbdbf5f6df676a9c66520d0d160967
Ansh-verma324 <ansh.verma@students.mq.edu.au>
2026-09-08 15:42:12 +1000

    Added more methods

--- src/Fire.java
+++ src/Fire.java
@@ -1,25 +1,36 @@
 public class Fire extends Entity {
-    int duration;
-    int intensity;
-    boolean burning;
+    private int duration;
+    private int intensity;
+    private Forest forest; // reference to the forest to access trees and other entities such as Trees, Lightning and Wind
 
-    public Fire(int x, int y, int duration, int intensity) {
+    public Fire(int x, int y, int duration, int intensity, Forest forest) {
         super(x, y);
         this.duration = duration;
         this.intensity = intensity;
-        this.burning = true;
+        this.forest = forest;
     }
 
     public void update(Tree tree, Wind wind){
-        if (burning_{
-            duration--;
-        })
 
     }
 
+    public void spread(){
 
+    }
 
-    int fireIntensity(){
-        return duration;
+    private boolean isAdjacent(Tree tree){
+
+    }
+
+    private double spreadChance(Tree tree){
+
+    }
+
+    private double WindInfluence(Wind wind, Tree tree){
+
+    }
+
+    public int fireIntensity(){
+        return intensity;
     }
 }
