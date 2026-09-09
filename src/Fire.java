c25c0148e85f35b2966181ca1d528a22bffa5214
Ansh-verma324 <ansh.verma@students.mq.edu.au>
2026-09-09 11:27:11 +1000

    Expanded update, spread, isAdjacent and spreadChance methods

--- src/Fire.java
+++ src/Fire.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class Fire extends Entity {
     private int duration;
     private int intensity;
@@ -11,22 +13,61 @@ public class Fire extends Entity {
     }
 
     public void update(Tree tree, Wind wind){
+        spread();
 
+        duration--;
+        if (duration <= 0){
+            setActive(false);
+        }
+ 
     }
 
     public void spread(){
+        List<Tree> treeList = forest.getTrees().getAll();
+
+        for(int i = 0; i < treeList.size(); i++){
+
+            Tree tree = treeList.get(i);
+            if (tree.getState() == Tree.TreeState.BURNING || tree.getState() == Tree.TreeState.BURNT) {
+                continue; // skip trees already burning or already burnt
+            }
+
+            if (!isAdjacent(tree)){
+                continue; //skip trees that are not adjacent to the fire
+            }
+
+            double chance = spreadChance(tree);
+            if (Math.random() < chance){
+                tree.setState(Tree.TreeState.BURNING);
+            }
+
+        }
+
 
     }
 
     private boolean isAdjacent(Tree tree){
-
+        int dx = Math.abs(tree.getX() - this.getX());
+        int dy = Math.abs(tree.getY() - this.getY());
+        return dx <= 1 && dy <= 1;
     }
 
     private double spreadChance(Tree tree){
+        double base = intensity/100;
+
+        double windBoost = 0;
+        List<Wind> windList = forest.getWinds.getAll();
+        for(int i = 0; i < windList.size(); i++){
+            windBoost += windInfluence(windList.get(i), tree);
+        }
+
+        return Math.min(base + windBoost, 1.0);
+
 
     }
 
     private double WindInfluence(Wind wind, Tree tree){
+
 
     }
 
