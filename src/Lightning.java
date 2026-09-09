fc3a59133839784ccb12edd50c8846bc4692adb0
cooper-w1 <315176122+cooperw2@users.noreply.github.com>
2026-09-09 21:50:05 +1000

    Added lightning cooldown and lightning strike count plus Updated operators

--- src/Lightning.java
+++ src/Lightning.java
@@ -1,3 +1,4 @@
+// import java.awt.Color;
 import java.util.List;
 
 public class Lightning extends Entity {
@@ -8,32 +9,57 @@ public class Lightning extends Entity {
     private boolean hasStruck;
     private boolean active = true;
     private Forest forest;
+
+    private int cooldownTicks = 0;
+    private int cooldownDuration;
+    private int totalStrikes = 0;
     
 
-    public Lightning(Position position, int duration, double strikeChance, double igniteChance, Forest forest) {
+    public Lightning(Position position, int duration, double strikeChance, double igniteChance, int cooldownDuration, Forest forest) {
         super(position);
+
+        if (strikeChance <0.0 || strikeChance > 1.0) {
+            throw new IllegalArgumentException ("strikeChance must be between 0.0 & 1.0");
+        }
+        if (igniteChance <0.0 || igniteChance > 1.0) {
+            throw new IllegalArgumentException ("ignite Chance must be between 0.0 & 1.0");
+        }
+        if (cooldownDuration < 0) {
+            throw new IllegalArgumentException ("cooldowDuration can't be negative");
+        }
         this.duration = duration;
         this.strikeChance = strikeChance;
         this.igniteChance = igniteChance;
+        this.cooldownDuration = cooldownDuration;
         this.hasStruck = false;
         this.forest = forest;
     }
 
     public void tick() {
-        hasStruck = Math.random() < strikeChance;
+
+        if (cooldownTicks > 0) {
+            cooldownTicks--;
+            hasStruck = false;
+        } else {
+            hasStruck = Math.random() < strikeChance;
 
         if (hasStruck) {
+            totalStrikes++;
+            cooldownTicks = cooldownDuration;
+
             List<Tree> trees = forest.getAllTrees();
             for (Tree tree : trees) {
                 strikeTree(tree);
             }
         }
-        
-        duration = duration - duration;
-        if (duration <=0) {
-            active = false;
-        }
     }
+        
+        duration--;
+        if (duration <= 0) {
+            active = false;
+            }
+        }
+    
     
 
     private void strikeTree(Tree tree) {
@@ -55,6 +81,10 @@ public class Lightning extends Entity {
 
     public int lightningIntensity() {
         return duration;
+    }
+
+    public int getTotalStrikes() {
+        return totalStrikes;
     }
 
 }
